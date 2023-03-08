12021bb4ac61d929d1301b31ee2ecc929293ce9e
Sarrouh09 <109944321+Sarrouh09@users.noreply.github.com>
2023-03-08 20:40:53 +0200

    First design

---
+++ app/src/main/java/com/example/passport_project/Payment_table.java
@@ -0,0 +1,14 @@
+package com.example.passport_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Payment_table extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_table);
+    }
+}
